2d7ca7bc633287dad7a8f95defe759ffb1d19ea1
Brenan Feld <nctarheelsfan8@yahoo.com>
2016-05-24 01:46:52 +0000

    add background

--- site.rb
+++ site.rb
@@ -1,10 +1,12 @@
-'require/bundle'
 require 'sinatra'
 ENV['PORT'] ||='4000'
 set :port, ENV['PORT']
 set :bind, '0.0.0.0'
 get '/' do
   erb :index
+end
+get '/italian' do
+  "Ciao!"
 end
 get '/read_more' do
   erb :read_more
